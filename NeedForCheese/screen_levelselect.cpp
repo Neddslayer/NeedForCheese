3e9047e4209f10f2bd7f357400baf4be46b7edb8
Neddslayer <neddslayer32@gmail.com>
2023-04-16 15:13:42 -0500

    try to use box2d

---
+++ NeedForCheese/screen_levelselect.cpp
@@ -0,0 +1,36 @@
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_mixer.h"
+#include "screens.h"
+#include "utilities.h"
+#include "box2d/box2d.h"
+
+static int finishScreen;
+
+b2Vec2 gravity(0.0f, -19.84f);
+b2World world(gravity);
+b2BodyDef playerDef;
+b2Body* playerBody;
+b2PolygonShape playerShape;
+b2FixtureDef playerFixtureDef;
+
+void InitLevelSelectScreen(void)
+{
+
+}
+void UpdateLevelSelectScreen(void)
+{
+
+}
+void DrawLevelSelectScreen(void)
+{
+
+}
+void UnloadLevelSelectScreen(void)
+{
+
+}
+int FinishLevelSelectScreen(void)
+{
+	return finishScreen;
+}
