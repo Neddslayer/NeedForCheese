0468159b186dad49f80b5ad2e19a3d9695471409
Neddslayer <neddslayer32@gmail.com>
2023-05-14 19:36:46 -0500

    comments :>

--- NeedForCheese/SDL_Camera.cpp
+++ NeedForCheese/SDL_Camera.cpp
@@ -5,6 +5,7 @@
 #include "utilities.h"
 #include "world.h"
 
+// Render a texture with a camera offset.
 int SDL_RenderCopy_Camera(Camera2D camera, SDL_Renderer* renderer, SDL_Texture* texture, SDL_Rect* srcrect, SDL_Rect* dstrect)
 {
     if (dstrect == NULL) return SDL_RenderCopy(renderer, texture, srcrect, dstrect);
@@ -12,6 +13,7 @@ int SDL_RenderCopy_Camera(Camera2D camera, SDL_Renderer* renderer, SDL_Texture* 
     return SDL_RenderCopy(renderer, texture, srcrect, &moddstrect);
 }
 
+// Render a texture with more customizability and a camera offset.
 int SDL_RenderCopyEx_Camera(Camera2D camera, SDL_Renderer* renderer, SDL_Texture* texture, SDL_Rect* srcrect, SDL_Rect* dstrect, double angle, const SDL_Point* center, SDL_RendererFlip flip)
 {
     if (dstrect == NULL) return SDL_RenderCopyEx(renderer, texture, srcrect, dstrect, angle, center, flip);
@@ -19,11 +21,13 @@ int SDL_RenderCopyEx_Camera(Camera2D camera, SDL_Renderer* renderer, SDL_Texture
     return SDL_RenderCopyEx(renderer, texture, srcrect, &moddstrect, angle, center, flip);
 }
 
+// Draw a line with a camera offset.
 int SDL_RenderDrawLine_Camera(Camera2D camera, SDL_Renderer* renderer, int x1, int x2, int y1, int y2)
 {
     return SDL_RenderDrawLine(renderer, x1 - camera.target.x - camera.offset.x, x2 - camera.target.x - camera.offset.x, y1 - camera.target.y - camera.offset.y, y2 - camera.target.y - camera.offset.y);
 }
 
+// Draw a rect with a camera offset.
 int SDL_RenderFillRect_Camera(Camera2D camera, SDL_Renderer* renderer, SDL_Rect* rect)
 {
     SDL_Rect modrect = { rect->x - camera.target.x - camera.offset.x, rect->y + camera.target.y + camera.offset.y, rect->w * camera.zoom.x, rect->h * camera.zoom.y };
@@ -50,7 +54,6 @@ Vector2f& Vector2f::operator-=(const Vector2f& other)
     y -= other.y;
     return *this;
 }
-
 Vector2d Vector2d::operator+(const Vector2d& other) const
 {
     return Vector2d(x + other.x, y + other.y);
